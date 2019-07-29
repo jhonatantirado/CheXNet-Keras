51528c1b04123e8fa5db90afeee39410a33ea32d
Jhonatan Tirado <jhonatantiradotiradodeep@gmail.com>
2019-07-29 17:51:03 -0500

    Channels first y last: en ambos casos da error

--- predict_pb.py
+++ predict_pb.py
@@ -19,6 +19,7 @@ def load_image(img_path, show=False):
 
     img = image.load_img(img_path, target_size=(224, 224))
     img_tensor = image.img_to_array(img,data_format='channels_first')# (channels, height, width)
+    # img_tensor = image.img_to_array(img)# (height, width, channels)
     img_tensor = np.expand_dims(img_tensor, axis=0)
     img_tensor /= 255.
 
