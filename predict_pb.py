6b119014b1864b4e57cb20d5c69a3eeb2c22b8ec
Jhonatan Tirado <jhonatantiradotiradodeep@gmail.com>
2019-07-31 21:34:50 -0500

    Usar cxr_dataset. No funciona aun

--- predict_pb.py
+++ predict_pb.py
@@ -3,6 +3,10 @@ import numpy as np
 import os
 from configparser import ConfigParser
 import tensorflow as tf
+import torch
+import cxr_dataset as CXR
+from torchvision import transforms, utils
+from torch.utils.data import Dataset, DataLoader
 
 def load_pb(path_to_pb):
     with tf.gfile.GFile(path_to_pb, 'rb') as f:
@@ -15,40 +19,50 @@ def load_pb(path_to_pb):
 def load_image(img_path, show=False):
 
     img = image.load_img(img_path, target_size=(224, 224))
-    # img_tensor = image.img_to_array(img,data_format='channels_first')# (channels, height, width)
     img_tensor = image.img_to_array(img)# (height, width, channels)
-    img_tensor = img_tensor.transpose(2,0,1)
     img_tensor = np.expand_dims(img_tensor, axis=0)
     img_tensor /= 255.
-
     return img_tensor
 
 if __name__ == "__main__":
 
-    config_file = "./config.ini"
-    cp = ConfigParser()
-    cp.read(config_file)
+    LABEL="Pneumonia"
+    STARTER_IMAGES=True
+    PATH_TO_IMAGES = "starter_images/"
+    POSITIVE_FINDINGS_ONLY=True
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    data_transform = transforms.Compose([
+        transforms.Scale(224),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
 
-    output_dir = cp["DEFAULT"].get("output_dir")
-    class_names = cp["DEFAULT"].get("class_names").split(",")
+    if not POSITIVE_FINDINGS_ONLY:
+        finding = "any"
+    else:
+        finding = LABEL
 
-    pb_model = cp["TRAIN"].get("pb_model")
-    pb_model_path = os.path.join(output_dir, pb_model)
+    dataset = CXR.CXRDataset(
+    path_to_images=PATH_TO_IMAGES,
+    fold='test',
+    transform=data_transform,
+    finding=finding,
+    starter_images=STARTER_IMAGES)
 
-    img_path_001 = 'data/images/00000344_000.PNG'
-    new_image_001 = load_image(img_path_001)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=1)
+    inputs, labels, filename = next(iter(dataloader))
+    dummy_input = torch.autograd.Variable(inputs.cpu())
 
+    pb_model_path='experiments/3/checkpoint2.tf.pb'
     graph = tf.Graph()
     with graph.as_default():
         print("** load model **")
         graph = load_pb(pb_model_path)
-        # print(graph.get_operations())
 
     with tf.Session(graph=graph) as sess:
-        input = graph.get_tensor_by_name("input:0")
-        feed_dict ={input:new_image_001}
-        # max_pool = graph.get_tensor_by_name("max_pool:0")
-        # max_pool = tf.transpose(max_pool, perm= [0,2,3,1])
-        op_to_restore = graph.get_operation_by_name("Sigmoid")
-        # print (sess.run(max_pool))
-        print (sess.run(op_to_restore,feed_dict))
+        output_tensor = graph.get_tensor_by_name('Sigmoid:0')
+        input_tensor = graph.get_tensor_by_name('input:0')
+        output = sess.run(output_tensor, feed_dict={input_tensor: dummy_input})
+        print(output)
