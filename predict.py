264fd4ba889fe8d9f5dee48b4ba3f7c0018aa393
Jhonatan Tirado <jhonatantiradotiradodeep@gmail.com>
2019-08-18 00:48:22 -0500

    First commit in Miau02

--- predict.py
+++ predict.py
@@ -16,7 +16,7 @@ def load_image(img_path, show=False):
     img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
 
     if show:
-        plt.imshow(img_tensor[0])                           
+        plt.imshow(img_tensor[0])
         plt.axis('off')
         plt.show()
 
@@ -43,12 +43,12 @@ if __name__ == "__main__":
     batch_size = cp["TEST"].getint("batch_size")
     test_steps = cp["TEST"].get("test_steps")
     use_best_weights = cp["TEST"].getboolean("use_best_weights")
-	
+
 	# parse weights file path
     output_weights_name = cp["TRAIN"].get("output_weights_name")
     weights_path = os.path.join(output_dir, output_weights_name)
     best_weights_path = os.path.join(output_dir, f"best_{output_weights_name}")
-	
+
     print("** load model **")
     if use_best_weights:
         print("** use best weights **")
@@ -64,9 +64,9 @@ if __name__ == "__main__":
         weights_path=model_weights_path)
 
     # image path
-    img_path_001 = 'data/images/00000001_000.PNG'
-    img_path_002 = 'data/images_002/images/00001336_000.PNG'
-    img_path_003 = 'data/images_003/images/00003923_014.PNG'
+    img_path_001 = 'starter_images/00001698_000.PNG'
+    img_path_002 = 'starter_images/00003728_000.PNG'
+    img_path_003 = 'starter_images/00005318_000.PNG'
     # load a single image
     new_image_001 = load_image(img_path_001)
     new_image_002 = load_image(img_path_002)
@@ -75,23 +75,23 @@ if __name__ == "__main__":
     pred_001 = model.predict(new_image_001)
     pred_002 = model.predict(new_image_002)
     pred_003 = model.predict(new_image_003)
-	
+
     print (pred_001)
     print (pred_002)
     print (pred_003)
-	
+
     result_001 = tf.argmax(pred_001, 1)
     result_002 = tf.argmax(pred_002, 1)
     result_003 = tf.argmax(pred_003, 1)
-	
+
     predicted_class_001 = tf.keras.backend.eval(result_001)
     predicted_class_002 = tf.keras.backend.eval(result_002)
     predicted_class_003 = tf.keras.backend.eval(result_003)
-	
+
     print (predicted_class_001)
     print (predicted_class_002)
     print (predicted_class_003)
-	
+
     print (class_names[predicted_class_001[0]])
     print (class_names[predicted_class_002[0]])
     print (class_names[predicted_class_003[0]])
